0bcbc07aa95cd7ccc8179a5b6245d45cde892494
ganesh <soccerganesh@gmail.com>
2020-10-28 22:59:22 +0530

    basic app setup complete

---
+++ blogs/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path,include
+from blogs import views
+
+urlpatterns = []
--- ecellwebsite/settings.py
+++ ecellwebsite/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'cap',
+    'blogs',
+    # 'courses',
+    'startups',
 ]
 
 MIDDLEWARE = [
