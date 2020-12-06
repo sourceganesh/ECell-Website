4a94b218430b62b66cd71959431565c83b12287e
gaurav2699 <gauravsingh2699@gmail.com>
2020-12-07 01:29:21 +0530

    Done with courses pages

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def indexView(request):
+    return render(request,'courses/index.html')
+
+def entrepreneurship(request):
+    return render(request,'courses/entrepreneurship.html')
+
+def finance(request):
+    return render(request,'courses/finance.html')
+
+def cryptocurrency(request):
+    return render(request,'courses/cryptocurrency.html')
