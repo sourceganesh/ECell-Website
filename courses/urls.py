a9e56f52ef23ecf163c66e9c4db537082c1f7d1f
gaurav2699 <gauravsingh.191it218@nitk.edu.in>
2021-02-24 12:16:04 +0530

    Added 2 new courses

--- courses/urls.py
+++ courses/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('entrepreneurship/', views.entrepreneurship, name="entrepreneurship"),
     path('finance/', views.finance, name="finance"),
     path('cryptocurrency/', views.cryptocurrency, name="cryptocurrency"),
+    path('productManagement/', views.productManagement, name="productManagement"),
+    path('entrepreneurship2/', views.entrepreneurship2, name="entrepreneurship2"),
 ]
