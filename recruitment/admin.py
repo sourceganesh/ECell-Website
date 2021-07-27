b9e17e136554ed8adb85febc9536e51afdf0a4fa
ganeshs <soccerganesh@gmail.com>
2021-07-28 01:23:38 +0530

    added export feature for rec models

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import ManagementTeamRecs, WebTeamRecs, MediaTeamRecs
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-admin.site.register(ManagementTeamRecs)
-admin.site.register(MediaTeamRecs)
-admin.site.register(WebTeamRecs)
+@admin.register(ManagementTeamRecs)
+class ManagementTeamRecsAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(MediaTeamRecs)
+class MediaTeamRecsAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(WebTeamRecs)
+class WebTeamRecsAdmin(ImportExportModelAdmin):
+    pass
