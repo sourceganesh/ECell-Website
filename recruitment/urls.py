ce8c40e897980011b18d478b49d279fee9f46771
ganeshs <soccerganesh@gmail.com>
2022-01-27 17:30:13 +0530

    rectruiment removed

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -5,7 +5,7 @@ app_name='recruitment'
 
 urlpatterns = [
     path('',views.index, name="index"),
-    path('management',views.management_form,name='management'),
-    path('media',views.media_form,name='media'),
-    path('web',views.web_form,name='web'),
+    # path('management',views.management_form,name='management'),
+    # path('media',views.media_form,name='media'),
+    # path('web',views.web_form,name='web'),
 ]
